5586957883b939b04c1b60f5551bc015588d5120
Logan Woolf <loganwoolf@gmail.com>
2022-01-21 00:37:15 +0000

    Implement fetchMyIp and callback handler

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const {fetchMyIP} = require('./iss.js');
 
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log('It didn\'t work\n', error);
+    return;
+  }
+  console.log(`IP Address: ${ip}`);
+});
 
+module.exports = { fetchMyIP };
